cd1af95fe27665f9e6a01a65265fd0df21e30f9a
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2024-06-16 17:03:29 -0300

    resource

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,4 +18,12 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Spring Boot', 'http
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2022-07-14T03:00:00.00Z', TIMESTAMP WITH TIME ZONE '2023-07-14T03:00:00.00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-08-14T03:00:00.00Z', TIMESTAMP WITH TIME ZONE '2023-09-14T03:00:00.00Z', 1);
 
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha Spring Boot', 'Trilha principal do curso', 1, 'https://th.bing.com/th/id/OIP.iHRuj81ROgUp9IUaw5um2gHaEO?w=295&h=180&c=7&r=0&o=5&pid=1.7', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas duvidas', 2, 'https://th.bing.com/th/id/OIP.iHRuj81ROgUp9IUaw5um2gHaEO?w=295&h=180&c=7&r=0&o=5&pid=1.7', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives excluisivas para a turma', 3, 'https://th.bing.com/th/id/OIP.iHRuj81ROgUp9IUaw5um2gHaEO?w=295&h=180&c=7&r=0&o=5&pid=1.7', 0, 1);
+
+
+
+
+
 
