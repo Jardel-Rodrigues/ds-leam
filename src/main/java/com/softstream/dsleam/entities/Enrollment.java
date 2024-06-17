6569974f21e6099cbc840a2389edd758238149eb
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2024-06-17 09:45:34 -0300

    Lessons seed, fix: lessonsDone

--- src/main/java/com/softstream/dsleam/entities/Enrollment.java
+++ src/main/java/com/softstream/dsleam/entities/Enrollment.java
@@ -2,12 +2,15 @@ package com.softstream.dsleam.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.softstream.dsleam.entities.pk.EnrollmentPK;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,7 +28,10 @@ public class Enrollment implements Serializable{
 	private Instant refundMoment;
 	private boolean available;
 	private boolean onlyUpdate;
-
+	
+	@ManyToMany(mappedBy = "enrollmentsDone")
+	private Set<Lesson> lessonsDone = new HashSet<>();
+	
 	public Enrollment() {
 
 	}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,14 +22,27 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas duvidas', 2, 'https://th.bing.com/th/id/OIP.iHRuj81ROgUp9IUaw5um2gHaEO?w=295&h=180&c=7&r=0&o=5&pid=1.7', 2, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives excluisivas para a turma', 3, 'https://th.bing.com/th/id/OIP.iHRuj81ROgUp9IUaw5um2gHaEO?w=295&h=180&c=7&r=0&o=5&pid=1.7', 0, 1);
 
-INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 1 ', 'Neste capitulo vamos comerça', 1, 'https://th.bing.com/th/id/OIP.iHRuj81ROgUp9IUaw5um2gHaEO?w=295&h=180&c=7&r=0&o=5&pid=1.7', 1, null);
-INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 2 ', 'Neste capitulo vamos continuar', 2, 'https://th.bing.com/th/id/OIP.iHRuj81ROgUp9IUaw5um2gHaEO?w=295&h=180&c=7&r=0&o=5&pid=1.7', 1, 1);
-INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 3 ', 'Neste capitulo vamos finalizar', 3, 'https://th.bing.com/th/id/OIP.iHRuj81ROgUp9IUaw5um2gHaEO?w=295&h=180&c=7&r=0&o=5&pid=1.7', 1, 2);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 1', 'Neste capitulo vamos comerça', 1, 'https://th.bing.com/th/id/OIP.iHRuj81ROgUp9IUaw5um2gHaEO?w=295&h=180&c=7&r=0&o=5&pid=1.7', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 2', 'Neste capitulo vamos continuar', 2, 'https://th.bing.com/th/id/OIP.iHRuj81ROgUp9IUaw5um2gHaEO?w=295&h=180&c=7&r=0&o=5&pid=1.7', 1, 1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 3', 'Neste capitulo vamos finalizar', 3, 'https://th.bing.com/th/id/OIP.iHRuj81ROgUp9IUaw5um2gHaEO?w=295&h=180&c=7&r=0&o=5&pid=1.7', 1, 2);
 
 INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2022-07-14T13:00:00.00Z', null, true, false);
 INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2022-07-14T13:00:00.00Z', null, true, false);
 
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Aula 1 capitulo 1', 1, 1);
+INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (1, 'Material de apoio: cap 1', 'https://www.youtube.com/watch?v=JvOxlcOnU-0');
 
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Aula 1 capitulo 2', 2, 1);
+INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (2, '', 'https://www.youtube.com/watch?v=JvOxlcOnU-0');
+
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Aula 1 capitulo 3', 3, 1);
+INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (3, '', 'https://www.youtube.com/watch?v=JvOxlcOnU-0');
+
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Tarefa do capitulo 1', 4, 1);
+INSERT INTO tb_task(id, description, question_Count, appoval_Count, weight, due_Date) VALUES (4, 'Fazer trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2024-06-14T13:00:00.00Z');
+
+INSERT INTO tb_lesson_done(lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lesson_done(lesson_id, user_id, offer_id) VALUES (3, 1, 1);
 
 
 
